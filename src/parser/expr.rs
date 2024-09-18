29eb4f1a21761a70798da663938c5d77c5b7c7f5
onotaizee@gmail.com <onotaizee@gmail.com>
2024-09-18 21:56:28 +0100

    Revert "chore: read file only on valid command input"
    This reverts commit 0a7188a09d74a9354788428f95dd2d4d8cb2234b.

--- src/parser/expr.rs
+++ src/parser/expr.rs
@@ -8,11 +8,10 @@ pub enum Expr {
 }
 
 
-impl Expr {
-	/// Method to accept expressions for printing
+impl ExprAccept for Expr {
 	fn accept(self) -> String {
 		match self {
-			Expr::Literal(x) => x.to_string(),
+			Expr::Literal(x) => x.accept(),
 			Expr::Unary{operator, right} => {
 				Self::parenthesize(operator.lexeme, vec![*right])
 			},
@@ -27,8 +26,8 @@ impl Expr {
 }
 
 impl Expr {
-	/// Parenthesize a given expression
-	pub fn parenthesize(name: String, exprs: Vec<Expr>) -> String
+	pub fn parenthesize<T>(name: String, exprs: Vec<T>) -> String
+		where T: ExprAccept
 	{
 		let mut builder = String::new();
 
@@ -47,7 +46,6 @@ impl Expr {
 	}
 }
 
-/// Storage for literal expressions
 pub enum ExprLiteral {
 	NUMBER(f64),
 	STRING(String),
@@ -68,35 +66,38 @@ impl ToString for ExprLiteral {
 		}
 	}
 }
+pub trait ExprAccept {
+	fn accept(self) -> String;
+}
+
+impl ExprAccept for ExprLiteral {
+	fn accept(self) -> String {
+		self.to_string()
+	}
+}
 
 /// `NEW` methods for ExprVariants
 impl Expr {
-	/// Create a new binary expression
 	pub fn new_binary(left: Expr, operator: Token, right: Expr) -> Self {
 		Expr::Binary{ left: Box::new(left), operator, right: Box::new(right) }
 	}
-	
-	/// Create a new unary expression
+
 	pub fn new_unary(operator: Token, right: Expr) -> Self {
 		Expr::Unary { operator, right: Box::new(right) }
 	}
-	
-	/// Create a new grouping expression
+
 	pub fn new_grouping(grouping: Expr) -> Self {
 		Expr::Grouping(Box::new(grouping))
 	}
-	
-	/// Create a new literal expression
+
 	pub fn new_literal(literal: ExprLiteral) -> Self {
 		Expr::Literal(literal)
 	}
 }
 
-/// In charge of printing expressions to stdout
 pub struct AstPrinter;
 
 impl AstPrinter {
-	/// Print an expression to stdout
 	pub fn print(expr: Expr) -> String{
 		return expr.accept()
 	}
