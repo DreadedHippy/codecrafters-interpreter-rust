b73f02c107e37d283d6a115f8c0ffab0a00b286d
onotaizee@gmail.com <onotaizee@gmail.com>
2024-09-18 21:34:32 +0100

    chore: Remove unneeded traits docs: Added documentation for expression

--- src/parser/expr.rs
+++ src/parser/expr.rs
@@ -8,10 +8,11 @@ pub enum Expr {
 }
 
 
-impl ExprAccept for Expr {
+impl Expr {
+	/// Method to accept expressions for printing
 	fn accept(self) -> String {
 		match self {
-			Expr::Literal(x) => x.accept(),
+			Expr::Literal(x) => x.to_string(),
 			Expr::Unary{operator, right} => {
 				Self::parenthesize(operator.lexeme, vec![*right])
 			},
@@ -26,8 +27,8 @@ impl ExprAccept for Expr {
 }
 
 impl Expr {
-	pub fn parenthesize<T>(name: String, exprs: Vec<T>) -> String
-		where T: ExprAccept
+	/// Parenthesize a given expression
+	pub fn parenthesize(name: String, exprs: Vec<Expr>) -> String
 	{
 		let mut builder = String::new();
 
@@ -46,6 +47,7 @@ impl Expr {
 	}
 }
 
+/// Storage for literal expressions
 pub enum ExprLiteral {
 	NUMBER(f64),
 	STRING(String),
@@ -66,38 +68,35 @@ impl ToString for ExprLiteral {
 		}
 	}
 }
-pub trait ExprAccept {
-	fn accept(self) -> String;
-}
-
-impl ExprAccept for ExprLiteral {
-	fn accept(self) -> String {
-		self.to_string()
-	}
-}
 
 /// `NEW` methods for ExprVariants
 impl Expr {
+	/// Create a new binary expression
 	pub fn new_binary(left: Expr, operator: Token, right: Expr) -> Self {
 		Expr::Binary{ left: Box::new(left), operator, right: Box::new(right) }
 	}
-
+	
+	/// Create a new unary expression
 	pub fn new_unary(operator: Token, right: Expr) -> Self {
 		Expr::Unary { operator, right: Box::new(right) }
 	}
-
+	
+	/// Create a new grouping expression
 	pub fn new_grouping(grouping: Expr) -> Self {
 		Expr::Grouping(Box::new(grouping))
 	}
-
+	
+	/// Create a new literal expression
 	pub fn new_literal(literal: ExprLiteral) -> Self {
 		Expr::Literal(literal)
 	}
 }
 
+/// In charge of printing expressions to stdout
 pub struct AstPrinter;
 
 impl AstPrinter {
+	/// Print an expression to stdout
 	pub fn print(expr: Expr) -> String{
 		return expr.accept()
 	}
