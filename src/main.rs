0a7188a09d74a9354788428f95dd2d4d8cb2234b
onotaizee@gmail.com <onotaizee@gmail.com>
2024-09-18 14:07:47 +0100

    chore: read file only on valid command input

--- src/main.rs
+++ src/main.rs
@@ -34,16 +34,21 @@ impl Lox {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
 
-        let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-            writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
-            String::new()
-        });
-
         match command.as_str() {
             "tokenize" => {
+                let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
+                    writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
+                    String::new()
+                });
+
                 Self::tokenize(file_contents.to_string());
             },
             "parse" => {
+                let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
+                    writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
+                    String::new()
+                });
+                
                 Self::parse(file_contents.to_string())
             }
             _ => {
